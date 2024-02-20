2d862420443137021572e94ae75d54708ce45281
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2024-02-19 22:35:47 -0500

    Update halcion_auto_calulator.py
    Fix import

--- hal_auto_check/halcion_auto_calulator.py
+++ hal_auto_check/halcion_auto_calulator.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.edge.options import Options
 import pyperclip
 import time
 
